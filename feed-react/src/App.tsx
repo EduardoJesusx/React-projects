f1d5f21fa870b7c37e0a14d39fd17c0500ad55fd
EduardoJesusx <srduduca@gmail.com>
2023-02-07 00:07:58 -0300

    feat-comment-area

--- feed-react/src/App.tsx
+++ feed-react/src/App.tsx
@@ -6,25 +6,14 @@ import styles from './App.module.scss'
 import { Sidebar } from './components/Sidebar'
 
 export function App() {
-
+ 
   return (
     <div>
       <Header/>
       <div className={styles.wrapper}>
         <Sidebar/>
         <main>
-          <Post 
-          author="teste " 
-          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit suscipit nam aliquid voluptate aperiam architecto rem at eveniet porro eligendi, quibusdam natus quasi reiciendis molestiae asperiores dolor veniam debitis quidem"
-          />
-          <Post 
-          author="teste " 
-          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit suscipit nam aliquid voluptate aperiam architecto rem at eveniet porro eligendi, quibusdam natus quasi reiciendis molestiae asperiores dolor veniam debitis quidem"
-          />
-          <Post 
-          author="teste " 
-          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit suscipit nam aliquid voluptate aperiam architecto rem at eveniet porro eligendi, quibusdam natus quasi reiciendis molestiae asperiores dolor veniam debitis quidem"
-          />
+          <Post/>
         </main>
       </div>
     </div>
--- feed-react/src/components/Post.tsx
+++ feed-react/src/components/Post.tsx
@@ -1,13 +1,35 @@
-interface PostProps {
-  author: string;
-  content: string;
-}
+import styles from './Post.module.scss'
 
-export function Post(props: PostProps) {
+export function Post() {
   return(
-    <>
-      <strong>{props.author} </strong><br/>
-      <p>{props.content}</p>
-    </>
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://robohash.org/asd"/>
+          <div className={styles.authorInfo}>
+            <strong>Nome</strong>
+            <span>Web Development</span>
+          </div>
+        </div>
+        <time dateTime=''></time>
+      </header>
+      <div className={styles.content}>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga veniam sit officia dolorum repudiandae vel repellendus consequuntur.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga veniam sit officia dolorum repudiandae vel repellendus consequuntur.</p>
+        <p><a href='#'> #Lorem ipsum dolor</a></p>
+        <p><a href='#'> #Lorem ipsum dolor</a></p>
+        <p><a href='#'> #Lorem ipsum dolor</a></p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu comentário</strong>
+        <textarea 
+          placeholder='Deixe seu comentário'
+        />
+        <footer>
+          <button type='submit'>Publicar</button>
+        </footer>
+      </form>
+    </article>
   )
 }
